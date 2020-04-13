783b18d36d33b48172d6b24f38438110efbd7705
Serdar Ayalp <serdarayalp@googlemail.com>
2020-04-13 17:15:22 +0200

    scanf

---
+++ Scanf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int meineZahl;
+    printf("Bitte geben Sie eine Zahl ein: ");
+    scanf("%d", &meineZahl);
+    printf("Die eingegebene Zahl ist: %d\n", meineZahl);
+}
