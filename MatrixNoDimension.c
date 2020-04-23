a09ff3c4aacddfd5ef600cfe83e9c729de3e0bbd
Serdar Ayalp <serdarayalp@googlemail.com>
2020-04-23 19:26:44 +0100

    MatrixNoDimension

--- MatrixNoDimension.c
+++ MatrixNoDimension.c
@@ -2,6 +2,34 @@
 
 int main()
 {
+
+    char _charMatrix[][20] = {"Computer", "Buch", "Stift"};
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%s\n", _charMatrix[i]);
+    }
+
+    printf("\n");
+
+    char monsterMatrix[][3][20] = {
+        {"Rat", "Bat", "Spider"},
+        {"Goblin", "Orc", "Dwarf"},
+        {"Dragon", "Lich", "Banshee"},
+        {"Demon", "Hydra", "Giant Spider"}};
+
+
+    for (int x = 0; x < 4; x++)
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            printf("%s\t", monsterMatrix[x][y]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
     // int intMatrix[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     char charMatrix[] = "Das ist ein Test";
