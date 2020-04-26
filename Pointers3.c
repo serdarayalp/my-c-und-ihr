3f67747b03d4740b87cefa92d63d5d0ef24ecfac
Serdar Ayalp <serdarayalp@googlemail.com>
2020-04-26 17:22:29 +0100

    Pointers

--- Pointers3.c
+++ Pointers3.c
@@ -2,33 +2,30 @@
 
 int main()
 {
+    int intMatrix[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int *p = intMatrix; // oder &intMatrix[0]
 
-    int *p;
-    int a = 100;
-    p = &a;
+    int intMatrix2[5][4];
+    int *p2 = &intMatrix2[0][0];
+    intMatrix2[4][3] = 27;
 
-    printf("Pointer beinhaltet die Adresse (Variable) ist: %p\n", p);
-    printf("Wert der Variable ueber den Pointer ist: %d\n", *p);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", intMatrix[i]);
+    }
 
+    printf("\n");
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", *(p + i));
+    }
+
+    printf("\n");
     printf("***************************************************************");
     printf("\n");
 
-    // p++; // Adresse im Pointer erhöht sich und damit ändert sich der Wert, auf den er zeigt
-    // ++p; // Adresse im Pointer erhöht sich und damit ändert sich der Wert, auf den er zeigt
-
-    // ++*p;   // der Wert, auf den der Pointer zeigt, erhöht sich. Adresse im Pointer ändert sich nicht
-    // ++(*p); // der Wert, auf den der Pointer zeigt, erhöht sich. Adresse im Pointer ändert sich nicht
-    // ++*(p); // der Wert, auf den der Pointer zeigt, erhöht sich. Adresse im Pointer ändert sich nicht
-
-    *p++;   // Pointer moves to the next int position (as if it was an array). But returns the old content
-    // (*p)++; // The value of ptr is incremented
-    // *(p)++; // Pointer moves to the next int position (as if it was an array). But returns the old content
-    // *++p;   // Pointer moves to the next int position, and then get's accessed, with your code, segfault
-    // *(++p); // Pointer moves to the next int position, and then get's accessed, with your code, segfault
-
-
-    printf("Pointer beinhaltet die Adresse (Variable) ist: %p\n", p);
-    printf("Wert der Variable ueber den Pointer ist: %d\n", *p);
+    printf("%d\n", *(p2 + (4 * 4) + 3));
 
     return 0;
 }
