cee30580dda599143b1a1696b39bbc13e408a172
S36379 <serdar.ayalp@regiocom.com>
2020-04-27 16:49:08 +0200

    Pointers

--- Pointers6.c
+++ Pointers6.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+
+void function1(char *text1, char *text2);
 
 int main()
 {
@@ -12,6 +15,25 @@ int main()
     char *charPointer = "Das ist auch ein Test...";
     printf("%s\n", charPointer);
 
+    char charMatrix1[50];
+    char charMatrix2[50];
+
+    printf("Bitte geben Sie Text1 ein (<50): ");
+    scanf("%s", charMatrix1);
+
+    printf("Bitte geben Sie Text1 ein (<50): ");
+    scanf("%s", charMatrix2);
+
+    function1(charMatrix1, charMatrix2);
 
     return 0;
+}
+
+void function1(char *text1, char *text2) {
+    char resultMatrix[100];
+
+    strcpy(resultMatrix, text1);
+    strcat(resultMatrix, text2);
+
+    printf("%s\n", resultMatrix);
 }
