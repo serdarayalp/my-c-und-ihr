17296a5ff97066e2a2a437ada9e324c1780cf59f
Semra Ayalp <serdarayalp@googlemail.com>
2020-05-22 18:35:22 +0200

    realloc

---
+++ realloc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char *str;
+
+    str = (char *)malloc(15);
+    strcpy(str, "tutorialspoint");
+    printf("String = %s,  Address = %u\n", str, str);
+
+    str = (char *)realloc(str, 25);
+    strcat(str, ".com");
+    printf("String = %s,  Address = %u\n", str, str);
+
+    free(str);
+
+    return (0);
+}
