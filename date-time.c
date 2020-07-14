5216b07cf7d68ddb5903005a6ca32c5a30616df7
Serdar Ayalp <serdarayalp@googlemail.com>
2020-07-14 16:43:39 +0200

    date time

---
+++ date-time.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+
+/*
+struct tm
+{ 
+  int  tm_sec;  	// Seconds: 0-60 (to accommodate leap seconds) 
+  int  tm_min;  	// Minutes: 0-59 
+  int  tm_hour; 	// Hours since midnight: 0-23 
+  int  tm_mday; 	// Day of the month: 1-31
+  int  tm_mon;  	// Months *since* January: 0-11 
+  int  tm_year; 	// Years since 1900 
+  int  tm_wday; 	// Days since Sunday (0-6) 
+  int  tm_yday; 	// Days since Jan. 1: 0-365 
+  int  tm_isdst;	// +1=Daylight Savings Time, 0=No DST, -1=unknown 
+};
+*/
+
+int main()
+{
+
+    return 0;
+}
